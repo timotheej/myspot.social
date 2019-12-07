a69de7e494791707668e5a42468431c05fa2a732
Timothée JEZEK <timothee.jezek@gmail.com>
2019-12-07 11:16:53 +0100

    test2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@ $config = [
 
   <title>{{ config('app.name') }}</title>
 
-  <!--   <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}"> -->
+  <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}">
 
 </head>
 
@@ -29,7 +29,7 @@ $config = [
 
 
   {{-- Load the application scripts --}}
-  <!--   <script src="{{ mix('dist/js/app.js') }}"></script> -->
+  <script src="{{ mix('dist/js/app.js') }}"></script>
 </body>
 
 </html>
